bd5c93cf13dc9880268734eab5418fd4b43463d5
josephxrex <Josephxrex.Joseph>
2023-01-19 17:58:09 -0600

    first commit

---
+++ src/pages/api/ordenes/[id].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  if (req.method === "POST") {
+    //console.log(req.query.id)
+    //console.log("Actualizando")
+    const { id } = req.query;
+    const ordenActualizada = await prisma.orden.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        estado: true,
+      },
+    });
+    res.status(200).json(ordenActualizada);
+  }
+}
